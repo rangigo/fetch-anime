f83975dc6b84da786f5982a53246f526bfff12f5
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-10-12 15:37:17 +0300

    Remove redux as the app's state is not complicated

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 
 import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-const composeEnhancers =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose
-
-const rootReducer = combineReducers({})
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
-
 const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
