ce25fabff22ad8cabf4867ff85363768623e26da
rangigo <phuoc.nguyen@nordpoolgroup.com>
2019-01-24 13:55:49 +0200

    add build & deploy path

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 const app = (
-  <BrowserRouter>
+  <BrowserRouter basename="fetch-anime">
     <App />
   </BrowserRouter>
 )
