335baafba5ed1f66edb0e79d07e65647bba227a0
Panigo <panigo3797@gmail.com>
2020-10-07 02:45:43 +0300

    Add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider } from '@apollo/react-hooks'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
