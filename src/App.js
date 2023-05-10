95df153ab5b0ccecb4c7b74b308d29a0af792217
Paavo <phuoc@hey.com>
2023-05-10 13:03:21 +0300

    Update season

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ class App extends Component {
         <div className={styles.Content}>
           <Switch>
             <Route exact path="/">
-              <Redirect to="/list/2021/spring" />
+              <Redirect to="/list/2023/spring" />
             </Route>
             <Route path="/list/genres/:genre/:type/:page" component={List} />
             <Route path="/list/:year/:season" component={List} />
             <Route path="*">
-              <Redirect to="/list/2021/spring" />
+              <Redirect to="/list/2023/spring" />
             </Route>
           </Switch>
         </div>
