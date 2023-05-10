09f15d31cb5a7c9a34d94fe7e5a24f7e231f6e5d
Paavo <phuoc@hey.com>
2023-05-10 13:06:03 +0300

    Update season navigation

--- src/components/Navigation/NavigationItem/index.js
+++ src/components/Navigation/NavigationItem/index.js
@@ -14,6 +14,8 @@ const NavigationItem = ({ season }) => {
         className={styles.DropdownContent}
         style={season === 'fall' ? { left: -59 } : {}}
       >
+        <Link to={`/list/2023/${season}`}>{season} 2023-2024</Link>
+        <Link to={`/list/2022/${season}`}>{season} 2022-2023</Link>
         <Link to={`/list/2021/${season}`}>{season} 2021-2022</Link>
         <Link to={`/list/2020/${season}`}>{season} 2020-2021</Link>
         <Link to={`/list/2019/${season}`}>{season} 2019-2020</Link>
